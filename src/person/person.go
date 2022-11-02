056764393e3ca4461ed926e7d5634f0f1e3692c4
Gittoman <anonyme.and.free@protonmail.com>
2022-11-02 07:01:06 +0200

    readfromfile function probably done

--- src/person/person.go
+++ src/person/person.go
@@ -2,7 +2,10 @@ package person
 
 import (
 	"encoding/json"
+	"errors"
 	"io/ioutil"
+	"log"
+	"renamer-tool/src/utility"
 	"strings"
 )
 
@@ -11,12 +14,17 @@ type Person struct {
 	Names []string `json:"Names"`
 }
 
+// Actresses class with all person members
 type Actresses struct {
 	ListOfActresses []Person `json:"Actresses"`
 }
 
 // New create new member of actor class
 func New(input string, runes []string) (Person, error) {
+	if len(input) == 0 || len(runes) == 0 {
+		err := errors.New("input string is empty")
+		log.Fatalln(err)
+	}
 	var names, oldNames []string
 	if len(input) > 0 {
 		names = strings.Split(input, string(runes[len(runes)-1]))
@@ -40,7 +48,12 @@ func SaveToFile(actorList []Person, filename string) {
 	// comment comment comment
 }
 
-// ReadFromFile function
-func ReadFromFile(filename string) []Person {
+// ReadFromFile function for reading actresses from generated json file
+func ReadFromFile(filename string) *Actresses {
 	file, err := ioutil.ReadFile(filename)
+	utility.CheckErrors(err)
+	actresses := new(Actresses)
+	err = json.Unmarshal(file, actresses)
+	utility.CheckErrors(err)
+	return actresses
 }
